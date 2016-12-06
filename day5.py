4ed3b24715b694fd0264e0be3acfbde19143d683
Markus Foote <markusfoote@gmail.com>
2016-12-05 17:24:03 -0700

    weak attempt at cinematic decryptic doesn't work yet

--- day5.py
+++ day5.py
@@ -1,5 +1,7 @@
 #/usr/bin/env python
 from hashlib import md5
+import sys
+import random 
 
 def day5a(code):
     doorcode = [];
@@ -12,6 +14,7 @@ def day5a(code):
         if num % 1000000 == 0:
             print num
     return doorcode
+
 def day5b(code):
     doorcode = ['_' for i in range(8)];
     posfillorder = []
@@ -24,11 +27,20 @@ def day5b(code):
             if pos in range(8) and pos not in posfillorder:
                 doorcode[pos] = val
                 posfillorder.append(pos)
+             #   sys.stdout.write('Decrypting: {}\r'.format(''.join(doorcode)))
+             #   sys.stdout.flush()
         num += 1
-        if num % 1000000 == 0:
-            print num
+#        if num % 1000000 == 0:
+#            print num
+#            tempstr = doorcode
+#            for i in range(8):
+#               if tempstr[i] == '_':
+#                    tempstr[i] = output[i+10]
+#            sys.stdout.write('\rDecrypting: {}'.format(''.join(tempstr)))
+#            sys.stdout.flush()
+    print num
     return doorcode
 
 if __name__ == '__main__':
     import sys
-    print day5b(sys.argv[1])
+    print ''.join(day5b(sys.argv[1]))
